991c286c08717f9e55f79cab9e168839f5dc0423
RabGh <rabiighais@gmail.com>
2023-03-01 00:23:10 +0200

    Error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,12 +7,12 @@ function Error() {
   <>
     <Header />
       <main>
-        <div className="error">
-          <h1>Error</h1>
-          <p>The word you are looking for doesn't exist! Or if it's another error, please contact us.</p>
-          <div className="error-buttons">
-            <button className="btn" onClick={() => {window.location.href='/'}}>Home</button>
-            <button className="btn" onClick={() => {window.location.href='/contact/'}}>Contact</button>
+        <div className="error-container">
+          <h1 className="error-message">Error</h1>
+          <p className="error-description">The word you are looking for doesn't exist! Or if it's another error, please contact us.</p>
+          <div className="error-btn-container">
+            <button className="error-btn" onClick={() => {window.location.href='/'}}>Home</button>
+            <button className="error-btn" onClick={() => {window.location.href='/contact/'}}>Contact</button>
           </div>
         </div>
       </main>
