b8ee06ded11e4f2d9b1e3b7f1edb888501d84ea5
RabGh <rabiighais@gmail.com>
2023-03-03 12:59:48 +0200

    Fix output results

--- src/pages/DictionaryApi.jsx
+++ src/pages/DictionaryApi.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import SearchButton from '../components/SearchButton'
 import SearchTextField from '../components/SearchTextField';
-import { Box, Container, Typography, Fade }from '@mui/material';
-// import { dictBox1, dictBox2, dictBox3, dictBox4, dictBox5 } from './../styles/home';
-// import { dictSpan1, dictSpan2, dictSpan3 } from './../styles/home';
-// import { typographyDict1, typographyDict2, typographyDict3 } from './../styles/home';
+import { Box, Container, Typography, Fade, List, ListItem, ListItemText }from '@mui/material';
 
 function DictionaryApi() {
   const [word, setWord] = useState('');
@@ -43,24 +40,15 @@ function DictionaryApi() {
   return (
     <Box sx={{ mr:2 }}>
       <Container maxWidth="md">
-        <Box sx={{ display: 'flex',
-                   alignItems: 'center',
-                   justifyContent: 'center', 
-                   }}>
-          <Typography variant="h4" sx={{ mr: 2 }}>
-
-          </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
           <Box sx={{ flexGrow: 1 }}>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <SearchTextField word={word} handleWordChange={handleWordChange} />
-            <Box sx={{ display: 'flex',
-                       justifyContent: 'center',
-                       mt: 2, 
-                       }}>
-              <SearchButton handleClick={() => fetchData(word)} isLoading={isLoading} isButtonLoading={isButtonLoading} />
-            </Box>
-          </form>
-        </Box>
+              <form onSubmit={(e) => e.preventDefault()}>
+                  <SearchTextField word={word} handleWordChange={handleWordChange} />
+                <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2, }}>
+                  <SearchButton handleClick={() => fetchData(word)} isLoading={isLoading} isButtonLoading={isButtonLoading} />
+                </Box>
+              </form>
+          </Box>
       </Box>
       {!isLoading && wordData && (
         <Fade in={true}>
@@ -69,46 +57,64 @@ function DictionaryApi() {
                      flexDirection: 'column',
                      alignItems: 'center',
                      justifyContent: 'center',
-                     ml: 6, 
+                     ml: 4, 
                      }}>
-            <Typography sx={{ mb:1 }}>
-            <Typography variant='h4'>{wordData.word}</Typography>
-            <Typography variant='h6' sx={{ ml: -1.5,
-                        fontStyle: 'italic', 
-                        }}>
+            <Box sx={{ mb:1 }}>
+            <Typography variant='h4'>
+              {wordData.word}
+            </Typography>
+            <Typography variant='body1' sx={{ color: '#FF7F50',
+                                              fontStyle: 'italic',
+                                              mr: 1 
+                                            }}>
                         ({wordData.part_of_speech})
             </Typography>
-        </Typography>
-        <ol>
-          {wordData.definitions.map((definition, index) => (
-            <li key={index}>
-              <Typography variant='body1' sx={{ display: 'flex',
-                                                alignItems: 'center',
-                                                justifyContent: 'center',
-                                                mb: 1, 
-                                                }}>
-                <Typography sx={{ fontWeight: 'bold',
-                                mr: 1,
-                                color: '#fc5185', 
-                                }}>
-                    {index + 1}. 
-                </Typography>
+            </Box>
+          <List>
+            {wordData.definitions.map((definition, index) => (
+              <ListItem key={index}>
+                <Box sx={{ display: 'flex',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          mb: 1, 
+                          }}>
+                  <Typography sx={{ fontWeight: 'bold',
+                                  mr: 1,
+                                  color: '#fc5185', 
+                                  }}>
+                      {index + 1}. 
+                  </Typography>
 
-                <Typography>
-                  {definition.charAt(0).toUpperCase() + definition.slice(1)}
-                </Typography>
+                  <Typography sx={{ color: '#00FFFF' }}>
+                    {definition.charAt(0).toUpperCase() + definition.slice(1)}
+                  </Typography>
 
+                </Box>
+              </ListItem>
+            ))}
+          </List>
+          {wordData.synonyms && (
+            <Typography variant='body1' sx={{ fontStyle: 'italic',
+                                              display: 'flex',
+                                              padding: '1rem', 
+                                            }}>
+              <Typography sx={{ fontWeight: 'bold', 
+                                color:"#fc5185", mr: 1 
+                              }}>
+              Synonyms: 
               </Typography>
-            </li>
-          ))}
-        </ol>
-        {wordData.synonyms && (
-          <Typography variant='body1' sx={{ fontStyle: 'italic', display: 'flex', padding: '1rem' }}>
-            <Typography sx={{ fontWeight: 'bold', color:"#fc5185", mr: 1 }}>
-            Synonyms: 
-            </Typography>
-            <Typography sx={{ color: 'red' }}>
-            {wordData.synonyms.join(', ').toUpperCase()}
+              <Typography sx={{ display: 'inline-flex' }}>
+              {wordData.synonyms.map((synonym, index) => (
+                <React.Fragment key={synonym}>
+                  <Typography sx={{ color: '#FF7F50' }}>
+                    {index !== 0 && ", "}
+                    {synonym
+                      .split(' ')
+                      .map((word) => word.charAt(0).toLocaleUpperCase() + word.slice(1))
+                      .join(' ')}
+                  </Typography>
+                </React.Fragment>   
+              ))}
             </Typography> 
           </Typography>
           )}
--- src/styles/home.js
+++ src/styles/home.js
@@ -20,123 +20,17 @@ export const SearchFieldDesign = {
   },
 };
 
-// export const SearchBtn = {
-//   marginLeft: '1rem',
-//   height: '3rem',
-//   width: '6rem',
-// };
-
 /* ---------- Header Styles ---------- */
-// export const headerStyles = {
-//   display: 'flex',
-//   justifyContent: 'space-between',
-// };
 
-// export const typographyHeader = {
-//   flexGrow: 1,
-// };
 
 
 /* ---------- Footer Styles ---------- */
-// export const footerStyles = {
-//   display: 'flex', 
-//   justifyContent: 'space-between',
-//   top: 'auto',
-//   bottom: 0,
-//   position: 'fixed',
-//   width: '100%',
-// };
 
-// export const fAppBarStyles = {
-//   top: 'auto',
-//   bottom: 0,
-// };
 
-// export const typographyCopyRight = {
-//   '&.typographyCopyRight': {
-//     color: 'black',
-//     flexGrow: 1,
-//   }
-// };
-
-// export const typographyL = {
-//   marginRight: '1rem',
-//   color: 'black'
-// };
 
 /* ---------- Home Styles ---------- */
 
-// export const homeContainer = {
-//   height: '100vh', 
-// };
 
-// export const boxHome1 = {
-//   display: 'flex', 
-//   flexDirection: 'column',
-//   justifyContent: 'center',
-//   alignItems: 'center',
-//   marginTop: 3,
-// }
-
-
-// export const boxHome2 = {
-//   mt: 3,
-// };
-
-// export const typographyHome1 = {
-//   mb: 2,
-// };
 
 /* ---------- Dictionary Styles ---------- */
 
-// export const dictBox1 = {
-//   mr:2,
-// };
-
-// export const dictBox2 = {
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'center',
-// };
-
-// export const typographyDict1 = {
-//   mr: 2,
-// };
-
-// export const dictBox3 = {
-//   flexGrow: 1,
-// };
-
-// export const dictBox4 = {
-//   display: 'flex',
-//   justifyContent: 'center',
-//   mt: 2,
-// };
-
-// export const dictBox5 = {
-//   mt: 2,
-// };
-
-// export const dictSpan1 = {
-//   ml: 1,
-//   fontStyle: 'italic',
-// };
-
-// export const typographyDict2 = {
-//   display: 'flex',
-//   alignItems: 'center',
-//   mb: 1,
-// };
-
-// export const dictSpan2 = {
-//   fontWeight: 'bold', 
-//   mr:1,
-// };
-
-// export const typographyDict3 = {
-//   fontStyle: 'italic',
-// };
-
-// export const dictSpan3 = {
-//   fontWeight: 'bold',
-// };
